cb8348d40d29b2bfe6f0db9736487ca0755b4bbe
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-12-24 12:34:27 -0500

    Add files via upload
    I finished writing the first method. Now, I will continue to the second method.

--- PigLatin.java
+++ PigLatin.java
@@ -1,5 +1,15 @@
 public class PigLatin{
   public static String pigLatinSimple(String s){
-    
+    String answer = "";
+    String first = s.substring(0,1);
+    if (first.equals("a") || first.equals("e") || first.equals("i") || first.equals("o") || first.equals("u")) {
+      answer += s;
+      answer += "hay";
+    } else {
+      answer += s.substring(1,s.length());
+      answer += first;
+      answer += "ay";
+    }
+    return answer;
   }
 }
