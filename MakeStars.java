117e3572c57ad0528578da970e490474d14b4b7b
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-12-24 12:26:32 -0500

    Add files via upload
    I have written my code for MakeStars, though I think there might be a mistake so I am fixing it.

--- MakeStars.java
+++ MakeStars.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+import java.util.Character;
+
 public class MakeStars{
+  Scanner sc = new Scanner(System.in);
+  String line = sc.nextLine();
+  Scanner lineSc = new Scanner(line);
+  String ans = "";
+  char val = '';
+  String white = "";
+  boolean whiteSpace = false;
+
+  while (lineSc.hasNext()) {
+    val = lineSc.next();
+    if (!Character.isWhitespace(val) && whiteSpace == false)  {
+      ans += "*";
+    } else if (!Character.isWhitespace(val) && whiteSpace == true){
+      ans += " *";
+      whiteSpace = false;
+    } else if (Character.isWhitespace(val)){
+      whiteSpace = true;
+    }
+  }
 
 }
