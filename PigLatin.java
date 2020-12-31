290b1168a3ab1bca62638ff0b671215867641660
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-12-31 17:41:07 -0500

    Add files via upload
    I finished writing up PigLatin Part II. I will now continue to PigLatin Part III.

--- PigLatin.java
+++ PigLatin.java
@@ -14,6 +14,27 @@ public class PigLatin{
   }
 
   public static String pigLatin(String s){
+    String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    String answer = "";
+    String first = s.substring(0, 2);
+    String one = s.substring(0, 1);
+    boolean start = false;
+
+    for (int i = 0; i < digraphs.size(); i++) {
+      if (first.equals(digraphs[i])) {
+        start = true;
+        break;
+      }
+    }
+
+    if (start) {
+      answer = s.substring(2, s.length()) + first + "ay";
+    } else if (one.equals("a") || one.equals("e") || one.equals("i") || one.equals("o") || one.equals("u")) {
+      answer = s + "hay";
+    } else {
+      answer = s.substring(1, s.length()) + "ay";
+    }
     
+    return answer;
   }
 }
